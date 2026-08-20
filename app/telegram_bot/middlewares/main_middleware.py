de3facd30ba645107bd53f54dbb697ac12e00fac
NIckB <schoolnick.ya@gmail.com>
2026-08-20 16:42:01 +0500

    feat(telegram): Add main middleware for Telegram bot
    - Adds MainMiddleware class to handle Telegram chat sessions
    - Provides database session and chat object injection for bot handlers
    - Ensures chat is created if not present
    - Restricts processing based on CHAT_ID settings

---
+++ app/telegram_bot/middlewares/main_middleware.py
@@ -0,0 +1,39 @@
+from collections.abc import Awaitable, Callable
+from typing import Any
+
+from aiogram import BaseMiddleware
+from aiogram.types import Chat as Tgchat
+from aiogram.types import Update
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
+
+from app.database.models import Chat
+from config import settings
+
+
+class MainMiddleware(BaseMiddleware):
+    def __init__(self, session_factory: async_sessionmaker[AsyncSession]):
+        self.session_factory = session_factory
+
+    async def __call__(
+        self,
+        handler: Callable[[Any, dict[str, Any]], Awaitable[Any]],
+        event: Update,
+        data: dict[str, Any],
+    ) -> Any:
+        tg_chat: Tgchat | None = data.get("event_chat")
+        if tg_chat is None:
+            return
+        if not settings.CHAT_ID or tg_chat.id in settings.CHAT_ID:
+            async with self.session_factory() as session:
+                chat = await session.get(Chat, tg_chat.id)
+                if not chat:
+                    chat = Chat(
+                        id=tg_chat.id,
+                    )
+                    session.add(chat)
+                    await session.commit()
+                data["chat"] = chat
+                data["session"] = session
+                data["chat_id"] = chat.id
+                await handler(event, data)
+                await session.commit()
